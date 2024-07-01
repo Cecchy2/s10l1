0dfbe509358c629b4ca975ede4799a511b9ae8b2
Cecchy2 <dariocecchinato@gmail.com>
2024-07-01 16:33:38 +0200

    finito esercizio ma non funziona

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,84 +1,27 @@
-import { Component } from "react";
+import { Col, Container, Row } from "react-bootstrap";
 import SingleBook from "./SingleBook";
-import fantasy from "../data/fantasy.json";
-import horror from "../data/horror.json";
-import history from "../data/history.json";
-import romance from "../data/romance.json";
-import scifi from "../data/scifi.json";
-import { Button, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import CommentArea from "./CommentArea";
 
-class BookList extends Component {
-  state = {
-    books: fantasy,
-    searchBar: "",
-  };
-
-  setBooks = (genere) => {
-    this.setState({ books: genere });
-  };
-
-  render() {
-    const filteredBooks = this.state.books.filter((book) =>
-      book.title.toLowerCase().includes(this.state.searchBar.toLowerCase())
-    );
-    console.log(filteredBooks);
-
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <Form className="text-start d-flex justify-content-center align-items-center">
-              <Form.Group className="w-50" controlId="formName">
-                <Form.Control
-                  type="text"
-                  placeholder="Cerca il tuo libro"
-                  value={this.state.searchBar}
-                  onChange={(event) => {
-                    event.preventDefault();
-                    this.setState({ searchBar: event.target.value });
-                  }}
-                />
-              </Form.Group>
-            </Form>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <Button variant="secondary" onClick={() => this.setBooks(fantasy)} className="m-4">
-              Fantasy
-            </Button>
-            <Button variant="secondary" onClick={() => this.setBooks(history)} className="m-4">
-              History
-            </Button>
-            <Button variant="secondary" onClick={() => this.setBooks(horror)} className="m-4">
-              Horror
-            </Button>
-            <Button variant="secondary" onClick={() => this.setBooks(romance)} className="m-4">
-              Romance
-            </Button>
-            <Button variant="secondary" onClick={() => this.setBooks(scifi)} className="m-4">
-              SciFi
-            </Button>
-          </Col>
-        </Row>
-        <Row>
-          {filteredBooks.map((book, index) => {
-            return (
-              <Col xs={12} md={6} xl={3} key={index}>
-                <SingleBook
-                  img={book.img}
-                  title={book.title}
-                  price={book.price}
-                  category={book.category}
-                  asin={book.asin}
-                />
+const BookList = ({ books, onCardClick, selectedCardId, recensioni }) => {
+  return (
+    <Container>
+      <Row>
+        <Col xs={8}>
+          <Row>
+            {books.map((book) => (
+              <Col xs={4} key={book._id}>
+                <SingleBook book={book} onCardClick={onCardClick} />
               </Col>
-            );
-          })}
-        </Row>
-      </Container>
-    );
-  }
-}
+            ))}
+          </Row>
+        </Col>
+        <Col xs={4}>
+          <h3 className="text-center">Book’s Reviews</h3>
+          {selectedCardId && <CommentArea recensioni={recensioni} selectedCardId={selectedCardId} />}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default BookList;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,28 +1,27 @@
 import { Component } from "react";
 import { Card } from "react-bootstrap";
-import CommentArea from "./CommentArea";
 
 class SingleBook extends Component {
   state = {
     selected: false,
   };
 
+  handleClick = () => {
+    this.setState({ selected: !this.state.selected });
+    this.props.onCardClick(this.props.book._id);
+  };
+
   render() {
+    const { book } = this.props;
     return (
-      <>
-        <Card
-          onClick={() => this.setState({ selected: !this.state.selected })}
-          className={this.state.selected ? "selected-card" : ""}
-        >
-          <Card.Img variant="top" src={this.props.img} />
-          <Card.Body>
-            <Card.Title>{this.props.title}</Card.Title>
-            <Card.Text>{this.props.price} &euro;</Card.Text>
-            <Card.Text>{this.props.category}</Card.Text>
-          </Card.Body>
-          {this.state.selected && <CommentArea asin={this.props.asin} />}
-        </Card>
-      </>
+      <Card onClick={this.handleClick} className={this.state.selected ? "selected-card" : ""}>
+        <Card.Img variant="top" src={book.img} />
+        <Card.Body>
+          <Card.Title>{book.title}</Card.Title>
+          <Card.Text>{book.price} &euro;</Card.Text>
+          <Card.Text>{book.category}</Card.Text>
+        </Card.Body>
+      </Card>
     );
   }
 }
